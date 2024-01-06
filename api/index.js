7694e839064f3ac615971d08dba33902203d67ea
Tariq <rafiqtariq19@gmail.com>
2024-01-06 19:45:44 +0530

    server for signup api designed and stored to database

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'; // npm i dotenv
 import testRouter from './routes/test.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL)
@@ -14,9 +15,12 @@ app.listen(3000, () => {
     console.log("Working fine!!")
 });
 
+app.use(express.json()) // By default server soesnot take json as input but using this function allows it to do so
+
 // app.get('/api/function/test', (req, res) => {
 //     res.send("test Server Working");
 // });
 // Using the above will cause us to use many lines when we are doing the same for many functions
 // So we split it into index -> routes -> controllers whenever we navigate to that particular server
 app.use('/api/function', testRouter)
+app.use('/api/auth', authRouter);
