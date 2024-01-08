72b386a1f0f10ab03bea954b558f63fbbeaeeb97
Tariq <rafiqtariq19@gmail.com>
2024-01-08 16:18:52 +0530

    Signup page designed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='max-w-lg mx-auto my-5'>
+    <h1 className='text-center text-3xl font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-2 '>
+        <input type='text' placeholder='username' className='p-2 rounded-lg border-solid border' id='username'/>
+        <input type='email' placeholder='email' className='p-2 rounded-lg border-solid border' id='email'/>
+        <input type='password' placeholder='password' className='p-2 rounded-lg border-solid border' id='password'/>
+        <button type='submit' className='bg-orange-400 p-2 rounded-lg disabled:opacity-80 hover:opacity-85'>Sign Up</button>
+      </form>
+      <div>
+        <div className='flex gap-2 mt-3'>
+          <p>Already a member</p> <span className='text-blue-500'>Sign In</span>
+        </div>
+      </div>
+    </div>
   )
 }
